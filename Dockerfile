08f449e4df9864489a9f6930ed4cc45acc362b02
Jussi Aalto <j.v.aalto@gmail.com>
2022-06-19 20:54:21 +0300

    working map

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM python:3.10-alpine
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+RUN pip3 install -r requirements.txt --no-cache-dir
 
 COPY . .
 
--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+import os
+
 app = Flask(__name__)
+GoogleMaps(app, key=os.environ["GOOGLE_API_KEY"])
 
 @app.route('/')
 def hello_huttunen():
-    return '<h1>Huttusta :D</h1>'
+    huttunen = Map(
+            identifier="huttunen",
+            varname="huttunen",
+            lat=60.1707253,
+            lng=24.9329029,
+            zoom=16
+    )
+    return render_template("index.html", huttunen=huttunen)
 
 if __name__ == "__main__":
     app.run(debug=True)
