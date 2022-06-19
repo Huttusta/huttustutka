40d05921880850db680e9a2b5af6dca7491604c7
Jussi Aalto <j.v.aalto@gmail.com>
2022-06-19 21:45:41 +0300

    marker test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 import os
@@ -12,9 +13,18 @@ def hello_huttunen():
     huttunen = Map(
             identifier="huttunen",
             varname="huttunen",
-            lat=60.1707253,
-            lng=24.9329029,
-            zoom=16
+            lat=60.1707368,
+            lng=24.9347073,
+            zoom=10,
+            markers=[
+                {
+                    "icon": url_for("static", filename="huttusukko40.png"),
+                    "lat": 60.1706704,
+                    "lng": 24.9348711,
+                    "infobox": "asdasd",
+                    "scale": 0.1
+                }
+            ]
     )
     return render_template("index.html", huttunen=huttunen)
 
