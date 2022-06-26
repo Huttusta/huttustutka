d33cf4a9556cf9274c852f402ca548de3a5f16df
Lauri Mauranen <lauri.mauranen@onebyte.fi>
2022-06-26 16:37:37 +0300

    google maps working with new stack

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,7 +1,17 @@
 import './assets/css/style.css'
+import { Loader } from 'google-maps'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
-
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
+  <div id="map"></div>
 `
+
+const loader = new Loader(process.env.GOOGLE_API_KEY, { version: "weekly" })
+
+let map: google.maps.Map
+loader.load().then(() => {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 60.1707368, lng: 24.9347073 },
+    zoom: 10,
+  })
+})
