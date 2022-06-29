1c357fcea27bdeaeebc2d1fa4216370997275d1a
Jussi Aalto <j.v.aalto@gmail.com>
2022-06-29 22:28:35 +0300

    move api key to env variable

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,23 +1,23 @@
-import './assets/css/style.css'
+import "./assets/css/style.css";
 import {
   createMap,
   initInfoWindow,
   initMarkers,
   MarkerStorage,
   addProductSelectorOnChange,
-} from "./map"
+} from "./map";
 
-const GOOGLE_API_KEY = "AIzaSyCrHBMGwEmJtbpZvNEsYfOUq6HVEopLdDQ"
-const MAP_ID = "map"
-const APP_ID = "app"
-const API_URL = "http://localhost:5000"
-const AMOUNTS_URL = `${API_URL}/amounts`
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+const MAP_ID = "map";
+const APP_ID = "app";
+const API_URL = "http://localhost:5000";
+const AMOUNTS_URL = `${API_URL}/amounts`;
 
 // global storage
-let markers: MarkerStorage
-let infoWindow: google.maps.InfoWindow
+let markers: MarkerStorage;
+let infoWindow: google.maps.InfoWindow;
 
-const map = await createMap(APP_ID, MAP_ID, GOOGLE_API_KEY)
-infoWindow = initInfoWindow()
-markers = await initMarkers(map, infoWindow, AMOUNTS_URL)
-addProductSelectorOnChange(map, infoWindow, markers, AMOUNTS_URL)
+const map = await createMap(APP_ID, MAP_ID, GOOGLE_API_KEY);
+infoWindow = initInfoWindow();
+markers = await initMarkers(map, infoWindow, AMOUNTS_URL);
+addProductSelectorOnChange(map, infoWindow, markers, AMOUNTS_URL);
