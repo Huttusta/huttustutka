f72b2a07c8b9442d338b2229170c565a31feb36f
Lauri Mauranen <lauri.mauranen@gmail.com>
2023-03-26 14:27:21 +0300

    cronille tehty oma kontti

--- backend/cron/paivita-tuotteet.sh
+++ backend/cron/paivita-tuotteet.sh
@@ -5,14 +5,6 @@ DATA_KANSIO="$2"
 
 cd "$TYOKANSIO"
 
-if [ ! -d "venv" ]; then
-  python3 -m venv venv
-  source venv/bin/activate
-  pip install -r requirements.txt
-else
-  source venv/bin/activate
-fi
-
 python3 scripts/get_products.py
 
 cp resources/products-sorted.json "$DATA_KANSIO"
